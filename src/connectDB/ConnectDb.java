cf6565ce8c1a76efc3b99ac82bb36d97285dbd95
ry-ushiyama <ry-ushiyama@virtualex.co,jp>
2020-05-18 18:58:48 +0900

    Add a function for connect database.

---
+++ src/connectDB/ConnectDb.java
@@ -0,0 +1,26 @@
+package connectDB;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConnectDb {
+	public static Map<String, String> loadDB(){
+		try {
+			// JDBCドライバのロード
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// ドライバが設定されていない場合はエラーになります
+			throw new RuntimeException(String.format("JDBCドライバのロードに失敗しました。詳細:[%s]", e.getMessage()), e);
+		}
+		// データベースにアクセスするために、データベースのURLとユーザ名とパスワードを指定
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String user = "library_admin";
+		String pass = "library_admin";
+		Map<String, String> connectinfo = new HashMap<String, String >();
+		connectinfo.put("url", url);
+		connectinfo.put("user", user);
+		connectinfo.put("pass", pass);
+
+		return connectinfo;
+	}
+}
