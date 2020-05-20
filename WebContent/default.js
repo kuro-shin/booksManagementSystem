9005cdda4fb509461141a033d1dfed868c49c784
kuroda_shintaro <sh-kuroda@virtualex.co.jp>
2020-05-20 12:00:46 +0900

    minor change

--- WebContent/default.js
+++ WebContent/default.js
@@ -1,42 +1,30 @@
 //var session = getSessionInfomation();
+var session = new Object();
+//session.employeeId='0001';
+//session.employeeName='未来太郎';
+//session.employeeRole='manager';
+session.employeeId='0002';
+session.employeeName='現在次郎';
+session.employeeRole='user';
 
-function executeAjax() {
+function display() {
 	 'use strict';
-	 var requestQuery = 1;
-	$.ajax({
-		type : 'POST',
-		dataType : 'json',
-		url : '/booksManagementSystem/GetLoginInfoServlet',
-		data : requestQuery,
-		async: false,
-		success : function(json) {
-			// サーバーとの通信に成功した時の処理
-			// 確認のために返却値を出力
-		//	if(json=="NEEDLOGIN"){
-//				alert('ログインしていません');
-//				location.href='./login.html';
-		//	}else{
+
+			if(session.employeeId!=null){
+				$('#userInformation').append('<p>社員ID:'+session.employeeId+'</p>');
+				$('#userInformation').append('<p>社員名:'+session.employeeName+'</p>');
 				$('#isUser').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">リクエスト一覧・申請</button>');
 				$('#isUser').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">借本一覧</button>');
 
-		//	if(ROLL=='manager'){
+			}
+			if(session.employeeRole='user'){
 				$('#isManager').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">書籍登録</button>');
 				$('#isManager').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">延滞者一覧</button>');
 				$('#isManager').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">図書管理者登録</button>');
-		//	}
-		//	}
+			}
 
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			// サーバーとの通信に失敗した時の処理
-			alert('displayするときにデータの通信に失敗しました');
 
-			console.log("XMLHttpRequest : " + XMLHttpRequest.status);
-			console.log("textStatus     : " + textStatus);
-			console.log("errorThrown    : " + errorThrown.message);
-			console.log(errorThrown)
-		}
-	});
+
 }
 
 
@@ -44,5 +32,6 @@ $(document).ready(function() {
 	// 初期表示用
 	//getSessionInfomation();
 	//executeAjax();
+	display();
 
 });
