3c877494303bf2270aafc29d082499b555c87517
kuroda_shintaro <sh-kuroda@virtualex.co.jp>
2020-05-20 15:17:01 +0900

    add DisplayBorrowingBooksServlet

--- WebContent/borrowingBooksDisplay/borrowingBooksDisplay.js
+++ WebContent/borrowingBooksDisplay/borrowingBooksDisplay.js
@@ -1,50 +1,78 @@
-function executeAjax() {
+//var session = getSessionInfomation();
+var session = new Object();
+//session.employeeId='0001';
+//session.employeeName='未来太郎';
+//session.employeeRole='manager';
+session.employeeId='0002';
+session.employeeName='現在次郎';
+session.employeeRole='user';
+
+function display() {
+	 'use strict';
+
+			if(session.employeeId!=null){
+				$('#userInformation').append('<p>社員ID:'+session.employeeId+'</p>');
+				$('#userInformation').append('<p>社員名:'+session.employeeName+'</p>');
+				$('#isUser').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">リクエスト一覧・申請</button>');
+				$('#isUser').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">借本一覧</button>');
+			}
+			if(session.employeeRole='manager'){
+				$('#isManager').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">書籍登録</button>');
+				$('#isManager').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">延滞者一覧</button>');
+				$('#isManager').append('<button onclick="location.href=\'http://localhost:8080/booksManagementSystem/newBookRegisration.html\'">図書管理者登録</button>');
+			}
+
+			 var requestQuery = 1;
+				$.ajax({
+					type : 'GET',
+					dataType : 'json',
+					url : '/booksManagementSystem/CheckDelinquentSevlet',
+					data : requestQuery,
+					async: false,
+					success : function(json) {
+						// サーバーとの通信に成功した時の処理
+						// 確認のために返却値を出力
+						var result = $.isEmptyObject(json);
+						console.log(result);
+						if(!result)
+						$('#checkDelinquent').append('返却期限を過ぎている本があります。速やかに返却してください。');
+
+					},
+					error : function(XMLHttpRequest, textStatus, errorThrown) {
+						// サーバーとの通信に失敗した時の処理
+						alert('checkDelinquentするときにデータの通信に失敗しました');
+
+						console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+						console.log("textStatus     : " + textStatus);
+						console.log("errorThrown    : " + errorThrown.message);
+						console.log(errorThrown)
+					}
+				});
+
+
+}
+
+function checkDelinquent() {
 	 'use strict';
 	 var requestQuery = 1;
 	$.ajax({
 		type : 'GET',
 		dataType : 'json',
-		url : '/BasicCheck/GetLoginInfoServlet',
+		url : '/booksManagementSystem/CheckDelinquentSevlet',
 		data : requestQuery,
 		async: false,
 		success : function(json) {
 			// サーバーとの通信に成功した時の処理
 			// 確認のために返却値を出力
-			if(json=="NOT LOGIN"){
-				$('#shainTableBody').append('<p>ログインしてください</p>');
-			}else{
-			for (var i = 0; i < json.length; i++) {
-				var s = json[i];
-				$('#shainTableBody').append('<tr id="shain_list'+(i+1)+'"><td id="id'+(i+1)+'">' + s.shain_id + '</td><td>' + s.shain_name + '</td><td>');
-
-				var l=s.loginuser;
-				if(ROLL=='manager'||l==true){
-				$('#shain_list'+(i+1)).append('<a id="edit'+(i+1)+'" href="http://localhost:8081/BasicCheck/EditShain.html?q='+s.shain_id+'">編集</a>');
-				}
-				$('#shain_list'+(i+1)).append('</td><td>');
-
-				if(ROLL=='manager'){
-				$('#shain_list'+(i+1)).append('<input type="button" value="削除" id="delete'+(i+1)+'" onclick="deleteShain(\''+s.shain_id+'\')" >');
-				}
-				$('#shain_list'+(i+1)).append('</td></tr>');
-
-				if(l==true){
-				$('#LoginName').append('ログインユーザー：'+USER);
-				}
-
-			}
-
-			if(ROLL=='manager'){
-				$('#createB').append('<button id="createbutton" onclick="location.href=\'http://localhost:8081/BasicCheck/createShain.html\'">社員新規作成</button>');
-			}
-
-			}
-
+			var result = $.isEmptyObject(json);
+			console.log(result);
+			if(!result)
+			$('#checkDelinquent').append('返却期限を過ぎている本があります。速やかに返却してください。');
 
 		},
 		error : function(XMLHttpRequest, textStatus, errorThrown) {
 			// サーバーとの通信に失敗した時の処理
-			alert('displayするときにデータの通信に失敗しました');
+			alert('checkDelinquentするときにデータの通信に失敗しました');
 
 			console.log("XMLHttpRequest : " + XMLHttpRequest.status);
 			console.log("textStatus     : " + textStatus);
@@ -54,3 +82,12 @@ function executeAjax() {
 	});
 }
 
+
+$(document).ready(function() {
+	// 初期表示用
+	//getSessionInfomation();
+	//executeAjax();
+	display();
+	checkDelinquent();
+
+});
