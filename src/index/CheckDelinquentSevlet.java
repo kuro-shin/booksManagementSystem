3567a65abb18e2fc546a3d1d50ec7aed6281d6f1
kuroda_shintaro <sh-kuroda@virtualex.co.jp>
2020-05-25 21:25:31 +0900

    minor change

--- src/index/CheckDelinquentSevlet.java
+++ src/index/CheckDelinquentSevlet.java
@@ -44,9 +44,8 @@ public class CheckDelinquentSevlet extends HttpServlet {
 
 
 		Map<String, String> conInfo = ConnectDb.loadDB();
-	//	Date date = new Date();
-  //      System.out.println(date.toString());
-
+//	Date date = new Date();
+//      System.out.println(date.toString());
 //      HttpSession session = request.getSession();
 //		String employeeId = (String) session.getAttribute("employeeId");
         String employeeId="0001"; //
